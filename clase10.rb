cd7e7fbcedd39c72f241e9d506aae2186cc1aaee
Angel Troncoso <angeltroncoso2019@outlook.es>
2024-08-23 15:36:17 -0400

    actualiza

---
+++ clase10.rb
@@ -0,0 +1,24 @@
+# colors = {'red' => '#cc0000', 'green' => 
+# '#00cc000', 'blue' => '#0000cc' }
+#  puts (colors.invert['#cc0000']) 
+
+#  colors_1 = {'red' => '#cc0000', 'green' => '#00cc000', 'blue' => '#0000cc' }
+#  colors_2 = {'white' => '#000000', 'pink' => '#FFB6C1'}
+#  puts (colors_1.merge(colors_2))
+
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8] 
+# puts numeros.group_by{ |numero| numero.even? }
+
+# colors_1 = {'red' => '#cc0000', 'green' => '#00cc000', 'blue' => '#0000cc' }
+#  colors_2 = {'white' => '#000000', 'pink' => '#FFB6C1'}
+#  print colors_1.to_a
+#  puts()
+
+# ventas = {
+#   Enero: 65000,
+#   Febrero: 68000,
+#   Marzo: 72000
+# }
+# puts ventas.values
+
+
