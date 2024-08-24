f5b6a3351d2402e0be77d85eea57f5af68b1fc86
Angel Troncoso <angeltroncoso2019@outlook.es>
2024-08-24 08:15:12 -0400

    actualiza

---
+++ api.rb
@@ -0,0 +1,17 @@
+require "uri"
+require "json"
+require "net/http"
+
+url = URI("http://www.omdbapi.com/?i=tt3896198&apikey=4edbf7df")
+
+http = Net::HTTP.new(url.host, url.port);
+request = Net::HTTP::Get.new(url)
+request["Content-Type"] = "application/json"
+request.body = JSON.dump({
+  "title": "Post 101",
+  "body": "Este es nuestro primer post",
+  "userId": 1
+})
+
+response = http.request(request)
+puts response.read_body
