d48a0e5222310dfb5ff73f502c2d43182c20c81c
Angel Troncoso <angeltroncoso2019@outlook.es>
2024-08-27 19:19:40 -0400

    actualiza

---
+++ clase11.rb
@@ -0,0 +1,30 @@
+require 'uri'
+require 'net/http'
+require 'json'
+
+def request(url_requested)
+url = URI(url_requested)
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+request = Net::HTTP::Get.new(url)
+
+request["cache-control"] = 'no-cache'#no aplica
+request["postman-token"] = '5f4b1b36-5bcd-4c49-f578-75a752af8fd5'#no aplica
+
+response = http.request(request)
+return JSON.parse(response.body)
+end
+data = request('https://jsonplaceholder.typicode.com/photos')[0..5]
+print data
+puts()
+
+photos = data.map{|x| x['url']}
+print photos
+
+html = ""
+photos.each do |photo|
+html += "<img src=\"#{photo}\">\n"
+end
+
+File.write('nuevo.html', html)
